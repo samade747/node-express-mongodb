bd27629611c6c783ac2c592c83a7333884c6587d
samade747 <samad.e747@gmail.com>
2024-01-20 09:24:04 +0500

    923

--- server.mjs
+++ server.mjs
@@ -1,1 +1,22 @@
 console.log('hellow world server side js ')
+
+import  express from "express"
+
+const app = express()
+const PORT = 8000
+
+// http://183.183.118.104:8000
+app.get('/', (req, res) => {
+    console.log('hello world!', new Date());
+    res.send('hello wordl' + new Date());
+})
+
+app.get('/profile', (req, res) => {
+    console.log('this is profile!', new Date());
+    res.send('this is profile' + new Date());
+})
+
+
+app.listen(PORT, () =>{
+    console.log(`example app listening on port ${PORT}`)
+})
